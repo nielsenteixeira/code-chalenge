0d93262156c250b968aeec1c9b40ad57726a5fa4
Nielsen Costa Teixeira <nielsen.teixeira@BAPBR-C02GT03D0KPF.local>
2023-01-02 16:54:38 -0300

    first challenges

---
+++ src/TwoSum/TwoSum.java
@@ -0,0 +1,21 @@
+package TwoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] run(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            } else {
+                numMap.put(nums[i], i);
+            }
+        }
+        return new int[] {};
+
+    }
+}
