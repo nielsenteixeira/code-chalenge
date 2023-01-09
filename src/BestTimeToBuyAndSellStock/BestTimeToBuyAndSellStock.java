fadd5a11220d4d8a4f769bae88088a169d9f8f0c
nielsenteixeira <nielsenteixeira@gmail.com>
2023-01-09 15:25:48 -0300

    Add Best Time do Buy and Sell

---
+++ src/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,15 @@
+package BestTimeToBuyAndSellStock;
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int diff = 0;
+        for (int i = 1; i < prices.length; i ++) {
+            int profit = prices[i] - min;
+            diff = Math.max(profit, diff);
+            min = Math.min(min, prices[i]);
+        }
+        return diff;
+    }
+}
